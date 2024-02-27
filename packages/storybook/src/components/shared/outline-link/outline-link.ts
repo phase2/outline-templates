fb20f5b7bf2b48e92d9fb352843f356abca3c5e4
Jake Strawn <jake.strawn@gmail.com>
2024-02-27 13:00:34 -0500

    chore(storybook): Fixes for utilizing `outline-core-link` as an extended component.

--- packages/storybook/src/components/shared/outline-link/outline-link.ts
+++ packages/storybook/src/components/shared/outline-link/outline-link.ts
@@ -1,9 +1,11 @@
 import { customElement } from 'lit/decorators.js';
-import { LitElement, html } from 'lit';
-// import { OutlineCoreLink } from '@phase2/outline-core-link';
+import { html } from 'lit';
+
+import { OutlineCoreLink } from '@phase2/outline-core-link';
 import globalStyles from './styles/outline-link.global.css?inline';
 import encapsulatedStyles from './styles/outline-link.encapsulated.css?inline';
 import { AdoptedStylesheets } from '@phase2/outline-adopted-stylesheets-controller';
+
 /**
  * Link component.
  *
@@ -13,7 +15,7 @@ import { AdoptedStylesheets } from '@phase2/outline-adopted-stylesheets-controll
  * @extends OutlineCoreLink
  */
 @customElement('outline-link')
-export class OutlineLink extends LitElement {
+export class OutlineLink extends OutlineCoreLink {
   GlobalStylesheets: AdoptedStylesheets | undefined = new AdoptedStylesheets(
     this,
     globalStyles,
@@ -29,10 +31,17 @@ export class OutlineLink extends LitElement {
       : undefined;
     return root;
   }
+
+  /**
+   * Overrides the render method from the parent class to wrap the rendered content
+   * in a div with the class "encapsulated-container". This demonstrates the difference
+   * between encapsulated and light DOM styling.
+   *
+   * @category rendering
+   * @returns {TemplateResult} The template to render.
+   */
   render() {
-    return html` <div class="encapsulated-container">
-      <slot></slot>
-    </div>`;
+    return html` <div class="encapsulated-container">${super.render()}</div> `;
   }
 }
 
